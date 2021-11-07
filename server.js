cd2800b5ddcbb44166716da05eb9d429a6b5e3dc
Skylar Harwell <swharwell@gmail.com>
2021-11-07 14:31:46 -0500

    Corrected Mongoose error

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
