7a6ed09e5540fc9c43de9820fa320e2a121e8640
Skylar Harwell <swharwell@gmail.com>
2021-11-07 14:44:46 -0500

    Heroku update

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const PORT = 3000;
 
 const app = express();
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/myFirstDatabase',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/myFirstDatabase',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   }
+// );
 
 app.use(logger("dev"));
 
@@ -25,10 +25,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
